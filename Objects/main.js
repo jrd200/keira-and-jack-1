f33237bcefa4957c9ded7f98a1b3acd3dc6c56de
Jack Denvir <jackdenvir@jackd.local>
2023-01-22 07:53:02 -0800

    put stuff with my this and that

---
+++ Objects/main.js
@@ -0,0 +1,22 @@
+const myHouse = {
+    stories: 2,
+    parking: false, 
+    bedrooms: 2,
+    garden: true, 
+    energy: "D",
+}
+const myCar = { 
+    color: "red", 
+    wheels: 4, 
+    steering: "power",
+    seats: 2, 
+    mpg: 20,
+
+}
+console.log(myCar)
+const myFavFilm = {
+    title: "Dark Knight", 
+    releaseYear: 2008,
+    lead: "C. Bale",
+    
+}
