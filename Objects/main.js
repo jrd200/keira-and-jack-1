81c33aa86996a9c0f88adf8a2a507dee574f3e50
keirastanley <keiraestanley@gmail.com>
2023-01-22 16:08:00 +0000

    Task 2 of objects complete

--- Objects/main.js
+++ Objects/main.js
@@ -13,10 +13,30 @@ const myCar = {
     mpg: 20,
 
 }
-console.log(myCar)
+
 const myFavFilm = {
     title: "Dark Knight", 
     releaseYear: 2008,
     lead: "C. Bale",
-    
 }
+
+let person = {
+    name: "Ben",
+    likesChocolate: true,
+};
+
+//If Ben likes chocolate, console.log("Ben loves chocolate")
+if (person.name === "Ben") {
+    if (person.likesChocolate) {
+        console.log("Ben loves chocolate")
+    }
+    else {
+        console.log("Ben hates chocolate")
+    }
+}
+else {
+    console.log("Wrong person!")
+}
+
+
+//The condition for the if, and the name Ben should both be read from the object using dot notation.
